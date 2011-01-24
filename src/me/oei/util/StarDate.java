b81e6390573e4ce2b489f9bef3d2fb5f07fad585
Chris Oei <chris@oei.me>
2011-01-24 11:02:04 -0800

    Added Java Concurrency In Practice (jcip) annotations.

--- src/me/oei/util/StarDate.java
+++ src/me/oei/util/StarDate.java
@@ -14,14 +14,17 @@ import java.util.GregorianCalendar;
 import java.util.TimeZone;
 import java.util.Locale;
 
+import net.jcip.annotations.*;
+
 import org.apache.log4j.*;
 
 // Note that all unit tests have been moved to a separate project (TestStarDate) so that the StarDate
 // jar files are as lean as possible.
 /**
  * @author Chris Oei
- *
+ * 
  */
+@ThreadSafe
 public final class StarDate implements Comparable<StarDate> {
 	/**
 	 * One second, measured in units of StarDate.
@@ -39,17 +42,22 @@ public final class StarDate implements Comparable<StarDate> {
 	 * One day, measured in units of StarDate.
 	 */
 	public static final double DAY = 0.0027379093;
-	
+
 	private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
 	private static final Locale LOCALE = Locale.ENGLISH;
 
 	private static final int MAX_YEAR_CACHED = 2100;
-	private static long START_OF_YEAR[] = new long[MAX_YEAR_CACHED + 1];
+	
+	@GuardedBy("itself")
+	private static long START_OF_YEAR[];
 	static { // Pre-fill cache with most commonly queried years.
-		START_OF_YEAR[2009] = 1230768000000L;
-		START_OF_YEAR[2010] = 1262304000000L;
-		START_OF_YEAR[2011] = 1293840000000L;
-		START_OF_YEAR[2012] = 1325376000000L;
+		synchronized (START_OF_YEAR) {
+			START_OF_YEAR = new long[MAX_YEAR_CACHED + 1];
+			START_OF_YEAR[2009] = 1230768000000L;
+			START_OF_YEAR[2010] = 1262304000000L;
+			START_OF_YEAR[2011] = 1293840000000L;
+			START_OF_YEAR[2012] = 1325376000000L;
+		}
 	}
 
 	private static Logger log = Logger.getLogger(StarDate.class);
@@ -65,7 +73,8 @@ public final class StarDate implements Comparable<StarDate> {
 	/**
 	 * Create a StarDate object using the time in milliseconds past the epoch.
 	 * 
-	 * @param timeInMillis the time in milliseconds past the epoch
+	 * @param timeInMillis
+	 *            the time in milliseconds past the epoch
 	 * @return a new StarDate object
 	 * 
 	 */
@@ -91,8 +100,7 @@ public final class StarDate implements Comparable<StarDate> {
 			int year = cal.get(Calendar.YEAR);
 			double y0 = getStartOfYear(year);
 			double y1 = getStartOfYear(year + 1);
-			return newInstance(year + (cal.getTimeInMillis() - y0)
-					/ (y1 - y0));
+			return newInstance(year + (cal.getTimeInMillis() - y0) / (y1 - y0));
 		}
 		return newInstance(cal.getTimeInMillis());
 	}
@@ -113,7 +121,9 @@ public final class StarDate implements Comparable<StarDate> {
 
 	/**
 	 * The StarDate copy factory method.
-	 * @param sd the StarDate object to copy
+	 * 
+	 * @param sd
+	 *            the StarDate object to copy
 	 * @return a new StarDate object
 	 */
 	public static StarDate newInstance(StarDate sd) {
@@ -122,7 +132,10 @@ public final class StarDate implements Comparable<StarDate> {
 
 	/**
 	 * Create a StarDate object using a string representation of the StarDate.
-	 * @param x the string representation of a StarDate (example: "2010.12345")
+	 * 
+	 * @param x
+	 *            the string representation of a StarDate (example:
+	 *            "2010.12345")
 	 * @return a new StarDate object
 	 */
 	public static StarDate parseStarDate(String x) {
@@ -144,6 +157,7 @@ public final class StarDate implements Comparable<StarDate> {
 
 	/**
 	 * Create a StarDate object using the current date/time.
+	 * 
 	 * @return a new StarDate object
 	 */
 	public static StarDate newInstance() {
@@ -151,13 +165,20 @@ public final class StarDate implements Comparable<StarDate> {
 	}
 
 	/**
-	 * @param tz	a TimeZone object
-	 * @param year	the (integer) year
-	 * @param month the month (January = 1, ..., December = 12)
-	 * @param day	the day (1 to 31 inclusive)
-	 * @param hour	the hour (0 to 23 inclusive)
-	 * @param minute the minute (0 to 59 inclusive)
-	 * @param second the second (0 to 59 inclusive)
+	 * @param tz
+	 *            a TimeZone object
+	 * @param year
+	 *            the (integer) year
+	 * @param month
+	 *            the month (January = 1, ..., December = 12)
+	 * @param day
+	 *            the day (1 to 31 inclusive)
+	 * @param hour
+	 *            the hour (0 to 23 inclusive)
+	 * @param minute
+	 *            the minute (0 to 59 inclusive)
+	 * @param second
+	 *            the second (0 to 59 inclusive)
 	 * @return a new StarDate object
 	 */
 	public static StarDate newInstance(TimeZone tz, int year, int month,
@@ -172,12 +193,13 @@ public final class StarDate implements Comparable<StarDate> {
 		cal.set(Calendar.SECOND, second);
 		return newInstance(cal);
 	}
-	
+
 	/**
-	 * Creates a new StarDate object from the last modification
-	 * time of File x.
-	 * @param x	the File object to examine.
-	 * @return	a new StarDate object
+	 * Creates a new StarDate object from the last modification time of File x.
+	 * 
+	 * @param x
+	 *            the File object to examine.
+	 * @return a new StarDate object
 	 */
 	public static StarDate newInstance(File x) {
 		return StarDate.newInstance(new Date(x.lastModified()));
@@ -187,7 +209,8 @@ public final class StarDate implements Comparable<StarDate> {
 	 * @param x
 	 *            the date in RFC2822 format (used in emails)
 	 * @return a new StarDate object
-	 * @throws ParseException if date cannot be parsed
+	 * @throws ParseException
+	 *             if date cannot be parsed
 	 */
 	public static StarDate parseRFC2822(String x) throws ParseException {
 		return StarDate.newInstance(new SimpleDateFormat(
@@ -198,23 +221,24 @@ public final class StarDate implements Comparable<StarDate> {
 	 * @param x
 	 *            The date in the format git uses
 	 * @return A new StarDate object
-	 * @throws ParseException if date cannot be parsed
+	 * @throws ParseException
+	 *             if date cannot be parsed
 	 */
 	public static StarDate parseGitDate(String x) throws ParseException {
 		return StarDate.newInstance(new SimpleDateFormat(
 				"EEE MMM dd HH:mm:ss yyyy Z").parse(x));
 	}
 
-
 	// ------------------------------------------------------------------------------------------------
 
 	// Clients should use the factory methods instead of the constructor.
-	// A private constructor and the lack of mutators makes this class immutable.
+	// A private constructor and the lack of mutators makes this class
+	// immutable.
 	private StarDate() {
 		y = 0;
 		throw new AssertionError();
 	}
-	
+
 	private StarDate(double x) {
 		y = x;
 		if (x < 1) {
@@ -288,26 +312,31 @@ public final class StarDate implements Comparable<StarDate> {
 	// Utilities
 	// ------------------------------------------------------------------------------------------------
 	private static long getStartOfYear(int y) {
-		if ((y >= 0) && (y <= MAX_YEAR_CACHED)) {
-			if (START_OF_YEAR[y] != 0) {
-//				log.info("cache hit for year " + y + ": " + START_OF_YEAR[y]);
-				return START_OF_YEAR[y];
-			} else if (y == 1970) {
-				// Special hack. Auto-initialized cache (zero) is by coincidence
-				// correct for 1970.
-				return 0;
-			} else {
-				Calendar cal = getDefaultCalendar();
-				cal.set(Calendar.YEAR, y);
-				cal.set(Calendar.MONTH, Calendar.JANUARY);
-				cal.set(Calendar.DAY_OF_MONTH, 1);
-				cal.set(Calendar.HOUR_OF_DAY, 0);
-				cal.set(Calendar.MINUTE, 0);
-				cal.set(Calendar.SECOND, 0);
-				cal.set(Calendar.MILLISECOND, 0);
-				START_OF_YEAR[y] = cal.getTimeInMillis();
-//				log.info("cache store for year " + y + ": " + START_OF_YEAR[y]);
-				return START_OF_YEAR[y];
+		synchronized (START_OF_YEAR) {
+			if ((y >= 0) && (y <= MAX_YEAR_CACHED)) {
+				if (START_OF_YEAR[y] != 0) {
+					// log.info("cache hit for year " + y + ": " +
+					// START_OF_YEAR[y]);
+					return START_OF_YEAR[y];
+				} else if (y == 1970) {
+					// Special hack. Auto-initialized cache (zero) is by
+					// coincidence
+					// correct for 1970.
+					return 0;
+				} else {
+					Calendar cal = getDefaultCalendar();
+					cal.set(Calendar.YEAR, y);
+					cal.set(Calendar.MONTH, Calendar.JANUARY);
+					cal.set(Calendar.DAY_OF_MONTH, 1);
+					cal.set(Calendar.HOUR_OF_DAY, 0);
+					cal.set(Calendar.MINUTE, 0);
+					cal.set(Calendar.SECOND, 0);
+					cal.set(Calendar.MILLISECOND, 0);
+					START_OF_YEAR[y] = cal.getTimeInMillis();
+					// log.info("cache store for year " + y + ": " +
+					// START_OF_YEAR[y]);
+					return START_OF_YEAR[y];
+				}
 			}
 		}
 		Calendar cal = getDefaultCalendar();
@@ -323,46 +352,47 @@ public final class StarDate implements Comparable<StarDate> {
 
 	private static Calendar getDefaultCalendar() {
 		Calendar cal = new GregorianCalendar(UTC, LOCALE);
-//		cal.clear();
+		// cal.clear();
 		return cal;
 	}
 
 	// Object overrides
 	// ----------------------------------------------------------------------------------------------
 
-	@Override public boolean equals(Object o) {
+	@Override
+	public boolean equals(Object o) {
 		// See Effective Java: 92.9/731
 		if (o instanceof StarDate) { // instanceof catches null case
-			return y == ((StarDate)o).y;
+			return y == ((StarDate) o).y;
 		}
 		return false;
 	}
-	
-	@Override public int hashCode() {
+
+	@Override
+	public int hashCode() {
 		// See Effective Java: 121.2/731
 		long l = Double.doubleToLongBits(y);
 		return (int) (l ^ (l >>> 32));
 	}
-	
+
 	/**
-	 * Returns the string representation of this StarDate.
-	 * The string consists of a 4-digit year, followed by a period, 
-	 * followed by exactly 13 digits (padded in back with zeros 
-	 * if necessary.) 
+	 * Returns the string representation of this StarDate. The string consists
+	 * of a 4-digit year, followed by a period, followed by exactly 13 digits
+	 * (padded in back with zeros if necessary.)
 	 **/
-	@Override public String toString() {
+	@Override
+	public String toString() {
 		return String.format("%.13f", Double.valueOf(y));
 	}
-	
-//	@Override public StarDate clone() {
-//		// See Effective Java: 135.3/731
-//		try {
-//			return (StarDate) super.clone();
-//		} catch(CloneNotSupportedException e) {
-//			throw new AssertionError(); //Can't happen
-//		}
-//	}
-	
+
+	// @Override public StarDate clone() {
+	// // See Effective Java: 135.3/731
+	// try {
+	// return (StarDate) super.clone();
+	// } catch(CloneNotSupportedException e) {
+	// throw new AssertionError(); //Can't happen
+	// }
+	// }
 
 	@Override
 	public int compareTo(StarDate x) {
