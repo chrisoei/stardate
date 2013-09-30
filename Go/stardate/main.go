1a4def75a3712e008976f78bd43e587800d923bf
Chris Oei <code@oei.io>
2013-09-30 16:16:28 -0700

    Add port listening for Go

--- Go/stardate/main.go
+++ Go/stardate/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/chrisoei/go-stardate"
 	"github.com/chrisoei/oei"
+	"net/http"
 	"os"
 	"time"
 )
@@ -13,16 +15,30 @@ func printtln(t time.Time) {
 	println(sd.String())
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	const lenPath = len("/stardate/")
+	f := "2006-01-02 15:04:05 MST"
+	t, err := time.Parse(f, r.URL.Path[lenPath:])
+	oei.ErrorHandler(err)
+	sd := stardate.New(t)
+	fmt.Fprintf(w, sd.String())
+}
+
 func main() {
 	var email = flag.Bool("email", false, "Use email date format")
 	var git = flag.Bool("git", false, "Use git date format")
 	var mtime = flag.Bool("mtime", false, "Use modification time of files")
+	var port = flag.Int("port", 0, "Port to listen on")
 	flag.Parse()
 	var f string
 	if *email {
 		f = "Mon, 2 Jan 2006 15:04:05 -0700"
 	} else if *git {
 		f = "Mon Jan 2 15:04:05 2006 -0700"
+	} else if *port > 0 {
+		portSpec := fmt.Sprintf(":%d", *port)
+		http.HandleFunc("/stardate/", handler)
+		http.ListenAndServe(portSpec, nil)
 	} else {
 		f = "2006-01-02 15:04:05 -0700"
 	}
