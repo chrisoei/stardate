3d94921f601e77d8b68897dff33b2b5ae2a2b7f6
Chris Oei <code@oei.io>
2014-07-13 14:03:28 -0700

    Add missing functions to .h file

--- C/stardate.h
+++ C/stardate.h
@@ -1,3 +1,6 @@
 
+double getStarDateFromTM(struct tm* x);
+double getStarDateFromTime(time_t t);
+double getStarDateFromTimeVal(struct timeval t);
+double getStarDateFromString(const char* fmt, const char* s);
 double getStarDateFromTimestamp(const char* ts);
-
