fc495c7151e0f98da332309cfbab2ad8542e9f38
Chris Oei <xcko-git@yahoo.com>
2014-09-30 09:42:52 -0700

    :  Add Clojure/Java 8 code A  Clojure/stardate.clj

---
+++ Clojure/stardate.clj
@@ -0,0 +1,17 @@
+(defn stardate 
+  ([zdt] ; if called with ZoneDateTime argument
+    (let [
+        utc (java.time.ZoneId/of "UTC")
+        get-start-of-year (fn [y]
+          (.toEpochSecond (java.time.ZonedDateTime/of y 1 1 0 0 0 0 utc)))
+        y (.getYear (.withZoneSameInstant zdt utc))
+        t0 (get-start-of-year y)
+        t1 (get-start-of-year (inc y))
+        t (.toEpochSecond zdt)
+      ]
+      (+ y (/ (double (- t t0)) (- t1 t0)))
+    ))
+  ([] ; if no arguments supplied, use now
+    (stardate (java.time.ZonedDateTime/now))
+  )
+)
