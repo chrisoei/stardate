44d5152ddce37b2f27d77321ae2c611eeda7832f
Chris Oei <xcko-git@yahoo.com>
2014-09-30 14:40:53 -0700

    Add scm to Clojure project.clj

--- Clojure/stardate/project.clj
+++ Clojure/stardate/project.clj
@@ -1,6 +1,12 @@
-(defproject stardate "0.1.0"
+(defproject stardate "0.1.1"
   :description "A library that converts to/from fractional years. Requires Java 8+."
   :url "http://github.com/chrisoei/stardate"
+  :scm {
+    :name "git"
+    :tag "0.1.1"
+    :url "https://github.com/chrisoei/stardate.git"
+    :dir "Clojure/stardate"
+  }
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
