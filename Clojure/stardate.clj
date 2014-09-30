f9e0810063f10bfd1e1733a77c10e03b6e858829
Chris Oei <xcko-git@yahoo.com>
2014-09-30 12:08:59 -0700

    :  Cleanup clojure code M  Clojure/stardate.clj

--- Clojure/stardate.clj
+++ Clojure/stardate.clj
@@ -2,8 +2,8 @@
   ([zdt] ; if called with ZoneDateTime argument
     (let [
         utc (java.time.ZoneId/of "UTC")
-        get-start-of-year (fn [y]
-          (.toEpochSecond (java.time.ZonedDateTime/of y 1 1 0 0 0 0 utc)))
+        get-start-of-year
+          #(.toEpochSecond (java.time.ZonedDateTime/of %1 1 1 0 0 0 0 utc))
         y (.getYear (.withZoneSameInstant zdt utc))
         t0 (get-start-of-year y)
         t1 (get-start-of-year (inc y))
