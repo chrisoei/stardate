1c0ffc8b37be2b50b02208a03e071cac7c3aff35
Chris Oei <xcko-git@yahoo.com>
2015-02-18 12:19:58 -0800

    Add error handling to starsrv

--- Clojure/starsrv/project.clj
+++ Clojure/starsrv/project.clj
@@ -1,4 +1,4 @@
-(defproject starsrv "1.0.0"
+(defproject starsrv "1.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- Clojure/starsrv/src/starsrv/core.clj
+++ Clojure/starsrv/src/starsrv/core.clj
@@ -10,14 +10,16 @@
          j (json/read-str b)
          k (first (keys j))
          v (first (vals j))]
-    (cond (= k "git") { "stardate" (stardate/ofGitFormat v) }
-          (= k "iso8601") { "stardate" (stardate/ofISO8601 v) }
-          (= k "now") { "stardate" (stardate/now) }
-          (= k "rfc2822") { "stardate" (stardate/ofRFC2822 v) }
-          (= k "stardate") { "zonedDateTime"
+    (try
+         (cond (= k "git") { "stardate" (stardate/ofGitFormat v) }
+               (= k "iso8601") { "stardate" (stardate/ofISO8601 v) }
+               (= k "now") { "stardate" (stardate/now) }
+               (= k "rfc2822") { "stardate" (stardate/ofRFC2822 v) }
+               (= k "stardate") { "zonedDateTime"
                              (.toString (stardate/toZonedDateTime
                                (Double/parseDouble v)
-                               "America/Los_Angeles"))})))
+                               "America/Los_Angeles"))})
+         (catch Exception e { "error" (.getMessage e) }))))
 
 (defn handler [request]
   {:status 200
