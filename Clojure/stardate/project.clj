315811b88104c3713a4cdef28a49ae82a24196a9
Chris Oei <github@chris-oei.com>
2015-07-02 04:36:36 +0000

    Bump Clojure stardate version to 3.6.1

--- Clojure/stardate/project.clj
+++ Clojure/stardate/project.clj
@@ -1,4 +1,4 @@
-(defproject stardate "3.6.0"
+(defproject stardate "3.6.1"
   :description "A library that converts to/from fractional years. Requires Java 8+."
   :url "http://github.com/chrisoei/stardate"
   :scm {
@@ -8,7 +8,7 @@
   }
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
 
 (when (< (.compareTo (System/getProperty "java.version") "1.8") 0)
   (println "Warning: need Java 8"))
