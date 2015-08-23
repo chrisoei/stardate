12914873158aca0445b708acc2752ccc878b1cf1
Chris Oei <github@chris-oei.com>
2015-08-22 22:46:39 -0700

    Bump clojure versions

--- Clojure/starsrv/project.clj
+++ Clojure/starsrv/project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [ring/ring-core "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [stardate "3.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [stardate "3.7.0"]]
   :main ^:skip-aot starsrv.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- Clojure/starsrv/test/starsrv/core_test.clj
+++ Clojure/starsrv/test/starsrv/core_test.clj
@@ -3,5 +3,5 @@
             [starsrv.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "FIXME, I do nothing."
+    (is true)))
